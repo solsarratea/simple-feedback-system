ee9d34f769c5c806121df8cbe25e426334236046
sol <mssarratea@gmail.com>
2021-05-29 01:21:28 -0300

    update scripts

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function initWebcamCapture(){
     if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia && video) {
         var constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
 
+
         navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
             video.srcObject = stream;
             video.play();
@@ -58,59 +59,82 @@ function setupBufferScene(){
     };
 
     ping = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, renderTargetParams );
-    pong = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, renderTargetParams);
+    pong = new THREE.WebGLRenderTarget( window.innerWidth, window.innerHeight, renderTargetParams );
 
 }
 
 var bufferUniforms, bufferMaterial, plane, bufferObject;
 function initBufferScene(){
+    bufferUniforms =  {
+        channel0: { type : 't', value : pong.texture },
+    }
+
     bufferMaterial = new THREE.ShaderMaterial({
-        uniforms : {
-            webcam: { type : 't', value : webcamTexture },
-            backbuffer:{ type : 't', value : webcamTexture},
-            time: {type: "f", value: 0.},
-            resolution : { type : 'v2', value : new THREE.Vector2( window.innerWidth, window.innerHeight) }
-        },
+        uniforms : bufferUniforms,
         vertexShader: document.getElementById( 'vertex' ).innerHTML,
-        fragmentShader: document.getElementById( 'frag' ).innerHTML
+        fragmentShader: document.getElementById( 'copy' ).innerHTML
     });
 
-    plane = new THREE.PlaneGeometry( 2, 2);
+    plane = new THREE.PlaneGeometry( 2, 2);  
     bufferObject = new THREE.Mesh( plane, bufferMaterial );
+    bufferObject.material.side = THREE.DoubleSide;
 
     bufferScene.add(bufferObject);
 }
 
-var finalMaterial,quad;
+var material,quad,geom;
 function initMainScene(){
-    finalMaterial =  new THREE.MeshBasicMaterial({map: ping});
-    quad = new THREE.Mesh( plane, finalMaterial );
+  material = new THREE.ShaderMaterial({
+      uniforms : {
+          webcam: { type : 't', value : webcamTexture },
+          backbuffer:{ type : 't', value : webcamTexture},
+          time: {type: "f", value: 0.},
+          resolution : { type : 'v2', value : new THREE.Vector2( window.innerWidth, window.innerHeight) }
+      },
+      vertexShader: document.getElementById( 'vertex' ).innerHTML,
+      fragmentShader: document.getElementById( 'frag' ).innerHTML
+});
+    finalMaterial =  new THREE.MeshBasicMaterial({ map: ping.texture });
+    geom = new THREE.PlaneBufferGeometry( 2, 2);
+    quad = new THREE.Mesh( geom, material );
+    quad.material.side = THREE.DoubleSide;
     scene.add(quad);
 }
-
 
 function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function render() {
-    bufferMaterial.uniforms.time.value = clock.getElapsedTime();
-    if(renderer.info.render.frame %2 == 0.){
-        //Draw scene to ping
-        renderer.setRenderTarget(ping);
-        renderer.render(bufferScene, camera);
+    quad.material.uniforms.time.value = clock.getElapsedTime();
+    if(renderer.info.render.frame %4 == 0.){
+    // Save current frame to pong
+    renderer.setRenderTarget(pong);
+    renderer.render(scene, camera);
+    renderer.setRenderTarget(null);
+    renderer.clear();
 
+    // Save buffer current frame to ping
+    renderer.setRenderTarget(ping);
+    renderer.render(bufferScene, camera);
+    renderer.setRenderTarget(null);
+    renderer.clear();
+
+    // Save current frame to pong
+        renderer.setRenderTarget(pong);
+        renderer.render(scene, camera);
         renderer.setRenderTarget(null);
         renderer.clear();
 
-        //Swap ping and pong
-        let temp = pong;
-        pong = ping;
-        ping = temp;
+        // Save buffer current frame to ping
+        renderer.setRenderTarget(ping);
+        renderer.render(bufferScene, camera);
+        renderer.setRenderTarget(null);
+        renderer.clear();
 
-        //Update uniforms
-        quad.material.map = ping;
-        bufferMaterial.uniforms.backbuffer.value=pong;
+    // Update channels
+    quad.material.uniforms.backbuffer.value = ping;
+    bufferMaterial.uniforms.channel0.value = pong;
 
    }
     //Render Main Scene
